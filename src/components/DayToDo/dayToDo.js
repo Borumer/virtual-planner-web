8acbd4d62883d2f33eca2355c13b612e9a346ec1
Varun <31084854+Borumer@users.noreply.github.com>
2021-04-24 19:00:34 -0400

    Implement DayToDo

---
+++ src/components/DayToDo/dayToDo.js
@@ -0,0 +1,25 @@
+export default function DayToDo(props) {
+    return (
+        <article>
+            <span>25</span>
+            <span>Monday</span>
+            <ul>
+                <li>Finish "The Great Gatsby" Constructed Response Outline</li>
+                <li>Study for Biology Unit 9 Quiz</li>
+                <li>Read pages 614-631 of "The New Economy" and "The New Culture"</li>
+                <li>Do a PopSugar Fitness workout</li>
+                <li>Polish C&CA beat for WIP</li>
+                <li>Review polar coordinates</li>
+                <li>Review 5A grammar topics and vocabulary</li>
+                <li>Practice new Symphonic Band music</li>
+            </ul>
+            <h3>After School</h3>
+            <ul>
+                <li>Test positional Kalman Filter</li>
+                <li>Finish implementing Positional Kalman Filter with process noise</li>
+                <li>Test Velocity Kalman Filter</li>
+                <li>Implement velocity Kalman Filter</li>
+            </ul>
+        </article>
+    );
+}
