a467f2f95273eca97f7ef567fcd0f0d9c2d7ce38
Varun <31084854+Borumer@users.noreply.github.com>
2021-04-24 18:59:07 -0400

    Implement WeeklyGoals

---
+++ src/components/WeeklyGoals/weeklyGoals.js
@@ -0,0 +1,23 @@
+import Goal from "../Goal/goal";
+import weeklyGoals from "./weeklyGoals.module.css";
+
+export default function WeeklyGoals(props) {
+	const goals = [
+		"Enroll in and pay for NYLF program",
+		"Order tennis team apparel",
+		"Finish Chapter 2 of Driver's Manual",
+		"Get good data for Ambient Noise Remover",
+		"Assemble hackathon team",
+	];
+
+	return (
+		<article>
+			<h1>GOALS AND TO-DO'S</h1>
+			<ul>
+				{goals.map(item => (
+					<li className={weeklyGoals.goal}><Goal goal={item} /></li>
+				))}
+			</ul>
+		</article>
+	);
+}
