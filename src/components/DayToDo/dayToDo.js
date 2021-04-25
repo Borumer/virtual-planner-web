12d253461b393b204cc785dfbcb5558a162f6a80
Varun <31084854+Borumer@users.noreply.github.com>
2021-04-25 10:32:55 -0400

    Finish

--- src/components/DayToDo/dayToDo.js
+++ src/components/DayToDo/dayToDo.js
@@ -1,94 +1,48 @@
 import dayToDo from "./dayToDo.module.css";
 import { daysOfWeek } from "../../lib/Moment";
+import { useFetch } from "../../lib/DataStore";
 import Goal from "../Goal/goal";
 
-/**
- *
- * @param {{dateStr : string}} props
- * @param {string} props.dateStr
- * @returns
- */
-export default function DayToDo({ dateStr = "" }) {
-	const getDateInfo = () => {
-		if (dateStr === "") {
-			const date = new Date();
-			return {
-				date: date.getDate(),
-				dayOfWeekName: daysOfWeek[date.getDay()],
-			};
-		}
+export default function DayToDo(props) {
+	const parseDate = props.year + "-" + props.month + "-" + props.date;
 
-		const dateComponents = dateStr.split("-");
-
-		const date = new Date(
-			dateComponents[0],
-			dateComponents[1],
-			dateComponents[2]
-		);
-
-		const dayOfWeekName = daysOfWeek[date.getDay()];
-		return {
-			date: date.getDate(),
-			dayOfWeekName,
-		};
-	};
-
-	const dateInfo = getDateInfo();
+	const dailyWork = useFetch("http://localhost:8000/work/daily?format=json");
 
 	return (
 		<article className={dayToDo.container}>
 			<h3>
-				<span>{dateInfo.date}</span>{" "}
-				<span>{dateInfo.dayOfWeekName}</span>
+				<span>{props.date}</span> <span>{daysOfWeek[props.day]}</span>
 			</h3>
 			<ul className={dayToDo.schoolwork}>
-				<li>
-					<Goal
-						goal={`Finish "The Great Gatsby" Constructed Response Outline`}
-					/>
-				</li>
-				<li>
-					<Goal goal="Study for Biology Unit 9 Quiz" />
-				</li>
-				<li>
-					<Goal
-						goal={`Read pages 614-631 of "The New Economy" and "The New
-					Culture`}
-					/>
-				</li>
-				<li>
-					<Goal goal="Do a PopSugar Fitness workout" />
-				</li>
-				<li>
-					<Goal goal="Polish C&CA beat for WIP" />
-				</li>
-				<li>
-					<Goal goal="Review polar coordinates" />
-				</li>
-				<li>
-					<Goal goal="Review 5A grammar topics and vocabulary" />
-				</li>
-				<li>
-					<Goal goal="Practice new Symphonic Band music" />
-				</li>
+				{dailyWork
+					? dailyWork
+							.filter(item => item.due_date === parseDate)
+							.filter(
+								item =>
+									item.type === "daily" || item.type === "day"
+							)
+							.map(item => (
+								<li>
+									<Goal {...item} />
+								</li>
+							))
+					: ""}
 			</ul>
 			<h3>After School</h3>
 			<ul className={dayToDo.afterSchoolWork}>
-				<li>
-					<Goal goal="Test positional Kalman Filter" />
-				</li>
-				<li>
-					<Goal
-						goal="Finish implementing Positional Kalman Filter with process
-					noise"
-					/>
-				</li>
-				<li>
-					<Goal goal="Test Velocity Kalman Filter" />
-				</li>
-				<li>
-					<Goal goal="Implement velocity Kalman Filter" />
-				</li>
+				{dailyWork
+					? dailyWork
+							.filter(item => item.due_date === parseDate)
+							.filter(
+								item =>
+									item.type === "after_school"
+							)
+							.map(item => (
+								<li>
+									<Goal {...item} />
+								</li>
+							))
+					: ""}
 			</ul>
 		</article>
 	);
--- src/components/DayToDoList/dayToDoList.js
+++ src/components/DayToDoList/dayToDoList.js
@@ -1,15 +1,39 @@
 import DayToDo from "../DayToDo/dayToDo";
 import dayToDoList from "./dayToDoList.module.css";
-import { useFetch } from "../../lib/DataStore";
 
 export default function DayToDoList(props) {
-	const work = useFetch("http://localhost:8000/work?format=json");
+	const currentWeek = (() => {
+		const now = new Date();
+		const firstDay = new Date(
+			now.getFullYear(),
+			now.getMonth(),
+			now.getDate() - now.getDay() + 1
+		);
+
+		const dates = [];
+
+		for (let i = 0; i < 7; i++) {
+			dates.push(
+				new Date(
+					firstDay.getFullYear(),
+					firstDay.getMonth(),
+					firstDay.getDate() + i
+				)
+			);
+		}
+
+		return dates;
+	})();
 
 	return (
 		<section className={`day-todo-list ${dayToDoList.container}`}>
 			<h2>Daily To-Do</h2>
 			<ul>
-				{work ? work.map(item => <li key={item.id}><DayToDo {...item} /></li>) : ""}
+				{currentWeek.map(item => (
+					<li key={item.getFullYear()}>
+						<DayToDo month={item.getMonth()} year={item.getFullYear()} date={item.getDate()} day={item.getDay()} />
+					</li>
+				))}
 			</ul>
 		</section>
 	);
